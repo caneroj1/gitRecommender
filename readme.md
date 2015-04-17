49330f0f669229027c4e840975618931064f2738
Kevin Bohinski <kbohinski@gmail.com>
2015-04-16 20:28:03 -0400

    I believe

---
+++ readme.md
@@ -0,0 +1,1 @@
+gitRecommender Final project for Artificial Intelligence. It is a recommender system that will suggest repositories you might be interested in.
---
+++ src/main/java/gitrecommender/Driver.java
@@ -0,0 +1,26 @@
+package gitrecommender;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/")
+public class Driver extends WebRequest {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter page = response.getWriter();
+	
+		page.print(returnHeader());
+		page.print("<div class='container'>");
+		page.print("<div class='row-fluid'>");
+		page.print("<div class='col-md-12'>");
+		page.print("<h1 class='text-center'>Git Recommender</h1>");
+		page.print("</div>");
+		page.print("</div>");
+		page.print("</div>");
+		page.print(returnFooter());
+	}
+}
