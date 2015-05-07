20680b9a5f0f13dd30d5e952b8e100cc78b743ee
Kevin Bohinski <kbohinski@gmail.com>
2015-05-06 23:23:51 -0400

    Dropdowns

--- src/main/java/gitrecommender/WebRequest.java
+++ src/main/java/gitrecommender/WebRequest.java
@@ -46,43 +46,21 @@ public class WebRequest extends HttpServlet {
 		return footer.toString();
 	}
 
-	public String createLink(String linkName, String linkUrl, int priority) {
+	public String createLink(String linkName, String linkUrl) {
 		/*
 		 * <button type="button"
 		 * class="btn btn-success btn-lg btn-block">" + linkName + "</button>
 		 */
-
-		if (priority == 1) {
-			return ("<div class=\"span12\"><div class=\"item\"><a href='"
-					+ linkUrl
-					+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block\">"
-					+ linkName + "</button>" + "</a></div></div><!--<a class=text-center href= + linkUrl + > + linkName + </a>-->");
-		} else if (priority == 2) {
-			return ("<div class=\"span10\"><div class=\"item\"><a href='"
-					+ linkUrl
-					+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block\">"
-					+ linkName + "</button>" + "</a></div></div><!--<a class=text-center href= + linkUrl + > + linkName + </a>-->");
-		} else if (priority == 3) {
-			return ("<div class=\"span8\"><div class=\"item\"><a href='"
-					+ linkUrl
-					+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block\">"
-					+ linkName + "</button>" + "</a></div></div><!--<a class=text-center href= + linkUrl + > + linkName + </a>-->");
-		} else if (priority == 4) {
-			return ("<div class=\"span6\"><div class=\"item\"><a href='"
-					+ linkUrl
-					+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block\">"
-					+ linkName + "</button>" + "</a></div></div><!--<a class=text-center href= + linkUrl + > + linkName + </a>-->");
-		} else if (priority == 5) {
-			return ("<div class=\"span4\"><div class=\"item\"><a href='"
-					+ linkUrl
-					+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block\">"
-					+ linkName + "</button>" + "</a></div></div><!--<a class=text-center href= + linkUrl + > + linkName + </a>-->");
-		} else {
-			return ("<div class=\"item\"><a href='"
-					+ linkUrl
-					+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block\">"
-					+ linkName + "</button>" + "</a></div><!--<a class=text-center href= + linkUrl + > + linkName + </a>-->");
-		}
+		return ("<div class=\"item\"><a href='"
+				+ linkUrl
+				+ "'><button type=\"button\" class=\"btn btn-success btn-lg btn-block dropdown-toggle\" type=\"button\" id=\"menu1\" data-toggle=\"dropdown\">"
+				+ linkName
+				+ "<span class=\"caret\"></span></button>"
+				+ "</a><ul class=\"dropdown-menu\" role=\"menu\" aria-labelledby=\"menu1\"><li role=\"presentation\">"
+				+ "<a role=\"menuitem\" tabindex=\"-1\" href=\"#\">LOLAI</a></li>"
+				+ "<li role=\"presentation\" class=\"divider\"></li>"
+				+ "<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\""
+				+ linkUrl + "\">View on GitHub</a></li></div>");
 	}
 
 	public String returnFormFieldWithLabel(String formIdentifier,
